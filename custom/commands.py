694e0a4452b05a93a95a36b24ba8cd3aee58490b
Andreas Krühlmann <andreaskruhlmann@gmail.com>
2016-03-29 16:39:51 +0200

    Added tool tip to commands.py

--- custom/commands.py
+++ custom/commands.py
@@ -1,5 +1,4 @@
-#sender = $username
-
+#These are the global comamnds for all channels by default. You can add your custom commands here or in your own database.
 command_list = {
 	"!fuckyou" : "Fuck you too $username DansGame",
 	"FeelsBadMan" : "FeelsBadMan",
